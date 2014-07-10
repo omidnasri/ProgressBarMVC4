c6eadf2911fa2714f8d9ee242e4611d070e2d124
Don Schenck <don.schenck@gmail.com>
2014-07-10 17:08:09 -0400

    Identity.Authenticate is working

--- ProgressBarMVC4/Controllers/IdentityController.cs
+++ ProgressBarMVC4/Controllers/IdentityController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using net.openstack.Providers.Rackspace;
+using net.openstack.Core.Domain;
 
 namespace ProgressBarMVC4.Controllers
 {
@@ -16,8 +18,29 @@ namespace ProgressBarMVC4.Controllers
             return View();
         }
 
-        public ActionResult Authenticate(string username, string apikey)
+        [HttpPost, ValidateInput(false)]
+        public ActionResult Authenticate(FormCollection formCollection)
         {
+            CloudIdentity cloudIdentity = new CloudIdentity()
+            {
+                APIKey = formCollection["apikey"],
+                Username = formCollection["username"]
+            };
+            try
+            {
+            CloudIdentityProvider cloudIdentityProvider = new CloudIdentityProvider(cloudIdentity);
+            UserAccess userAccess = cloudIdentityProvider.Authenticate(cloudIdentity);
+            ViewBag.LastOperation = "Authentication successful";
+            ViewBag.username = formCollection["username"];
+            ViewBag.apikey = formCollection["apikey"];
+            ViewBag.isAuthenticated = true;
+            }
+            catch (Exception e)
+            {
+                ViewBag.isAuthenticated = false;
+                ViewBag.LastOperation = e.Message;
+            }
+
             return View("Index");
         }
     }
