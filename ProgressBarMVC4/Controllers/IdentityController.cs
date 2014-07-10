7d0de428bec420663fe3fab45fb595445aef0322
Don Schenck <don.schenck@gmail.com>
2014-07-10 15:27:16 -0400

    Added IdentityController and associated Identity Index view

---
+++ ProgressBarMVC4/Controllers/IdentityController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProgressBarMVC4.Controllers
+{
+    public class IdentityController : Controller
+    {
+        //
+        // GET: /Identity/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
